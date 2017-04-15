9ed0ee0fd3e2c4e31b9fc533f4b9079934d84a21
sunil <skhiatani@gmail.com>
2017-04-15 13:48:23 +0800

    add structure to angular app clean up javascript using jshint

---
+++ bindings.js
@@ -0,0 +1,1 @@
+app.controller('GraphController', app.GraphController);
---
+++ config.js
@@ -0,0 +1,17 @@
+var app = angular.module('app', ['chart.js']);
+
+// Optional configuration
+app.config(['ChartJsProvider', function (ChartJsProvider) {
+	'use strict';		
+	// Configure all charts
+	ChartJsProvider.setOptions({
+	chartColors: ['#FF5252', '#FF8A80'],
+	responsive: true
+	});
+	// Configure all line charts
+	ChartJsProvider.setOptions('line', {
+	showLines: false
+	});
+}])
+;
+
