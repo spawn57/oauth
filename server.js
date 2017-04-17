cf9bc4fc8e2be04c986d24b828056bcde9345553
sunil <skhiatani@gmail.com>
2017-04-17 22:33:26 +0800

    forgot to update server.js

--- server.js
+++ server.js
@@ -1,12 +1,72 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var GoogleAuth = require('google-auth-library');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+var session = require('express-session');
+
+app.use(session({secret:'aksfjzxkv82asjhfd2as124dw82'}));
+
+// used for google's authentication
+var CLIENT_ID = '860177011019-ti6sgag4jgcf3hmcr54trmmqkdofoee8.apps.googleusercontent.com';
+var auth = new GoogleAuth;
+var client = new auth.OAuth2(CLIENT_ID, '', '');
+
+// used to generated guest names
 var userCount = 0;
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+})); 
 
 app.get('/', function (req, res, next) {
 	res.sendFile(__dirname + '/client/index.html')
+});
+
+app.get('/main.html', function (req, res, next) {	
+	if (req.session.user === undefined) {
+		res.status(403).send('You need to sign in <a href="http://localhost:7777">here</a>');
+		return;
+	}
+	
+	// authenticate the token again if it's there.
+	try {
+		var token = req.session.user.token;
+		client.verifyIdToken(token, CLIENT_ID, function(e, login) {			
+			var payload = login.getPayload();
+			// authenticated, send them to the page
+			res.sendFile(__dirname + '/client/main.html');
+			return
+		});
+	}
+	catch (e) {
+		// delete the user in the session because it's not valid anymore and fail
+		delete req.session.user;
+		res.status(403).send('You need to sign in again <a href="http://localhost:7777">here</a>');
+	};
+}); 
+
+app.post('/token', function (req, res, next) {
+	var token = req.body.idtoken;
+	console.log(token);	
+	client.verifyIdToken(token, CLIENT_ID, function(e, login) {
+		var payload = login.getPayload();
+		var user = {};
+		user.token = token;
+		user.userid = payload['sub'];
+		user.name = payload['name'];
+		user.email = payload['email'];
+		req.session.user = user;
+		res.status(201).send(user.name);
+	});
+});
+
+app.post('/signout', function (req, res, next) {
+	req.session.destroy();
+	res.send('session destroyed OK');
 });
 
 app.use(express.static('client'));
@@ -33,5 +93,5 @@ io.on('connection', function (client) {
 	});
 });
 
-
 server.listen(7777);
+
